4c637d1dc87a4c76fdc7a87b1d2be5657dee6768
gromo <gromovoy88@gmail.com>
2024-09-29 18:53:16 +0200

    Remove content component, add lock file

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import type { Page } from './types/strapi';
+
+const { find } = useStrapi<Page>();
+
+const { data } = useAsyncData('page', () => find('page'));
+</script>
+
 <template>
-  <StrapiContent />
+  <div v-if="data?.data?.[0]">
+    <StrapiPageRenderer :page="data.data[0].attributes" />
+  </div>
 </template>
